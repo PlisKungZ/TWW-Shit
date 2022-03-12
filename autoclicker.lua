6d16efd8e32f76f1d07f6dd2765833594ff082e1
PlisKungZ <78639515+PlisKungZ@users.noreply.github.com>
2022-03-12 21:22:46 +0700

    Delete autoclicker.lua

--- autoclicker.lua
+++
@@ -1,6 +0,0 @@
-while true do
-    wait()
-local VirtualInputManager = game:GetService("VirtualInputManager")
-VirtualInputManager:SendMouseButtonEvent(1552, 983, 0, true, game, 1)
-VirtualInputManager:SendMouseButtonEvent(1552, 983, 0, false, game, 1)
-end
